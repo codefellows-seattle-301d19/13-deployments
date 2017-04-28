ff0854b02d7619d9d3bc9dcd2e3403ad223c1e59
janousek77 <janousek77@gmail.com>
2017-04-28 09:33:18 -0700

    figured out environment stuff

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy'); // REVIEW: We've added a new package here to our requirements, as well as in the package.json
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3200;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://postgres:1234@localhost:5432/kilovolt'; // TODO: Don't forget to set your own conString
+const conString = `${process.env.DATABASE_URL}` + 'kilovolt'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
