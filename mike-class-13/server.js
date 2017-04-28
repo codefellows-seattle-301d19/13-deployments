0649d7004635d141f75e0b5d0f5737ef676e0114
Adutchguy <mdm_8@hotmail.com>
2017-04-28 11:24:37 -0700

    bia??

--- mike-class-13/server.js
+++ mike-class-13/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const requestProxy = require('express-request-proxy'); // REVIEW: We've added a new package here to our requirements, as well as in the package.json
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = process.env.DATABASE_URL + 'kilovolt';
+const conString = process.env.HEROKU_POSTGRESQL_GREEN_URL + 'kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
