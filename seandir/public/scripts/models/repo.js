5d238a82aba447c6556158e127d3839c67cbd1a1
Sean Reigle <sereigle@gmail.com>
2017-04-28 22:08:27 -0700

    added stuff

--- seandir/public/scripts/models/repo.js
+++ seandir/public/scripts/models/repo.js
@@ -10,10 +10,9 @@
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.get('/github/user/repos?type=owner')
+    $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
-
   };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
